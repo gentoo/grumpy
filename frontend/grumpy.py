40953ed104a9f49316360e261fa45bf7ff8ed57b
Mart Raudsepp <leio@gentoo.org>
2016-12-04 09:09:18 +0200

    frontend: add a /setup/ page for listing existing projects and devs
    Later we can convert this into a form to choose which projects/devs to follow;
    hence the "setup" naming.

--- frontend/grumpy.py
+++ frontend/grumpy.py
@@ -1,5 +1,6 @@
 from flask import render_template, request
 from flask_classy import FlaskView
+from sqlalchemy.sql import collate
 
 from backend.lib import models
 
@@ -10,3 +11,7 @@ class GrumpyView(FlaskView):
     def index(self):
         categories = models.Category.query.all()
         return render_template("index.html", categories=categories)
+
+    def setup(self):
+        maintainers = models.Maintainer.query.order_by(collate(models.Maintainer.email, 'NOCASE')).all()
+        return render_template("setup.html", maintainers=maintainers)
