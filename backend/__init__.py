85f13c2845302158fb1853e34d095a122d84ac53
Mart Raudsepp <leio@gentoo.org>
2016-12-05 23:11:56 +0200

    frontend: Setup Flask-WTF and use it for following maintainer checkboxes display
    No POST handling yet.

--- backend/__init__.py
+++ backend/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask("frontend") # FIXME: Finish rearranging frontend/backend modules properly instead of pretending to be frontend in backend/__init__ because jinja templates are looked for from <what_is_passed_here>/templates
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///../backend/grumpy.db" # FIXME: configuration support; weird ../ because of claiming we are "frontend" to Flask and want to keep the path the same it was before for now. But this problem should go away with config, at least for postgres :)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'Change me, you fool'
 db = SQLAlchemy(app)
 
 from frontend import *
--- frontend/grumpy.py
+++ frontend/grumpy.py
@@ -1,9 +1,19 @@
 from flask import render_template, request
 from flask_classy import FlaskView
 from sqlalchemy.sql import collate
+from flask_wtf import FlaskForm
+from wtforms import SelectMultipleField, widgets
+
 
 from backend.lib import models
 
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+class FollowSetupForm(FlaskForm):
+    maintainers = MultiCheckboxField('Followed maintainers', coerce=int)
 
 class GrumpyView(FlaskView):
     route_base='/'
@@ -14,4 +24,14 @@ class GrumpyView(FlaskView):
 
     def setup(self):
         maintainers = models.Maintainer.query.order_by(collate(models.Maintainer.email, 'NOCASE')).all()
-        return render_template("setup.html", maintainers=maintainers)
+        form = FollowSetupForm()
+        choices = []
+        form_mapping = {}
+        for maintainer in maintainers:
+            choices.append((maintainer.id, maintainer.email))
+            form_mapping[maintainer.id] = maintainer
+
+        form.maintainers.choices = choices
+        form.process()
+
+        return render_template("setup.html", mapping=form_mapping, form=form)
